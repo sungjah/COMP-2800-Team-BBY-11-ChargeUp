f8f9ceff79fa88534dc53e400b0de78be8fb8491
SungJae Hong <sungjaehong10@gmail.com>
2021-05-13 03:25:47 -0700

    implemented map api and charging station database to the map.

--- js/map.js
+++ js/map.js
@@ -1,29 +1,384 @@
-mapboxgl.accessToken =
-    'pk.eyJ1Ijoic3VuZ2phaCIsImEiOiJja25hMmpocDAwOWpsMndtaTRoanAzNXYwIn0.IxR5TqT3_wnNcwW33kKlkA';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-});
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-    enableHighAccuracy: true
-})
+ mapboxgl.accessToken =
+     'pk.eyJ1Ijoic3VuZ2phaCIsImEiOiJja25hMmpocDAwOWpsMndtaTRoanAzNXYwIn0.IxR5TqT3_wnNcwW33kKlkA';
+ var geojson = {
+     "type": "FeatureCollection",
+     "features": [{
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.100262444394, 49.222495514519]
+         },
+         "properties": {
+             "address": "6810 Main Street",
+             "lot_operator": "City of Vancouver",
+             "geo_local_area": "Sunset"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.1371, 49.2904]
+         },
+         "properties": {
+             "address": "900 Denman St",
+             "lot_operator": "Easy Park Lot",
+             "geo_local_area": "West End"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.113686386031, 49.2624805074924]
+         },
+         "properties": {
+             "address": "455 West 10th Ave",
+             "lot_operator": "Easy Park Lot",
+             "geo_local_area": "Mount Pleasant"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.106549, 49.243656]
+         },
+         "properties": {
+             "address": "4575 Clancy Loranger Way",
+             "lot_operator": "CoV",
+             "geo_local_area": "Riley Park"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.106978, 49.212004]
+         },
+         "properties": {
+             "address": "26 Marine Drive SW",
+             "lot_operator": "Canadian Tire",
+             "geo_local_area": "Marpole"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.112943, 49.28775]
+         },
+         "properties": {
+             "address": "999 Canada Place Way",
+             "lot_operator": "Canada Place",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.142173, 49.283155]
+         },
+         "properties": {
+             "address": "Beach Ave. @ Cardero St.",
+             "lot_operator": "Easy Park / Park Board",
+             "geo_local_area": "West End"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.1239, 49.26036]
+         },
+         "properties": {
+             "address": "890 W.12th Ave.",
+             "lot_operator": "Vancouver General Hospital",
+             "geo_local_area": "Fairview"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.152652, 49.273767]
+         },
+         "properties": {
+             "address": "1305 Arbutus St.",
+             "lot_operator": "City of Vancouver",
+             "geo_local_area": "Kitsilano"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.116602, 49.280369]
+         },
+         "properties": {
+             "address": "788 Richards St.",
+             "lot_operator": "Impark",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.129085, 49.226886]
+         },
+         "properties": {
+             "address": "6469 Oak St.",
+             "lot_operator": "Telus / Retailer",
+             "geo_local_area": "Oakridge"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.1173, 49.283672]
+         },
+         "properties": {
+             "address": "701 W. Georgia St.",
+             "lot_operator": "Easy Park Lot",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.117592, 49.233385]
+         },
+         "properties": {
+             "address": "650 W.41st Ave.",
+             "lot_operator": "Ivanhoe Cambridge",
+             "geo_local_area": "Oakridge"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.115042, 49.283543]
+         },
+         "properties": {
+             "address": "555 Seymour St,",
+             "lot_operator": "BCIT / Impark",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.137209, 49.278736]
+         },
+         "properties": {
+             "address": "Beach Ave. @ Bute St.",
+             "lot_operator": "Easy Park / Park Board",
+             "geo_local_area": "West End"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.12074, 49.286919]
+         },
+         "properties": {
+             "address": "1055 Eveleigh St.",
+             "lot_operator": "Ivanhoe Cambridge",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.124668, 49.277692]
+         },
+         "properties": {
+             "address": "1100 Granville St.",
+             "lot_operator": "Best Western",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.115606, 49.279426]
+         },
+         "properties": {
+             "address": "775 Hamilton St.",
+             "lot_operator": "Easypark",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.071246, 49.276066]
+         },
+         "properties": {
+             "address": "1661 Napier St.",
+             "lot_operator": "VSB",
+             "geo_local_area": "Grandview-Woodland"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.101866, 49.279306]
+         },
+         "properties": {
+             "address": "180 Keefer St.",
+             "lot_operator": "Easypark",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.04354, 49.286447]
+         },
+         "properties": {
+             "lot_operator": "Pacific National Exhibition",
+             "geo_local_area": "Hastings-Sunrise"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.12546941683, 49.2902878170643]
+         },
+         "properties": {
+             "address": "480 Broughton Street",
+             "lot_operator": "Coal Harbour Community Centre",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.185974, 49.268372]
+         },
+         "properties": {
+             "address": "2083 Alma St.",
+             "lot_operator": "Jericho Village",
+             "geo_local_area": "West Point Grey"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.1196416, 49.28700382]
+         },
+         "properties": {
+             "address": "1040 W. Pender St.",
+             "lot_operator": "Ivanhoe Cambridge",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.134491, 49.204022]
+         },
+         "properties": {
+             "address": "1200 W.73rd Ave.",
+             "lot_operator": "ASQ Building Ltd",
+             "geo_local_area": "Marpole"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.130193, 49.30005]
+         },
+         "properties": {
+             "lot_operator": "Vancouver Aquarium",
+             "address": "845 Avison Way"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.139793, 49.281483]
+         },
+         "properties": {
+             "address": "Beach Ave. @ Broughton St.",
+             "lot_operator": "Easy Park / Park Board",
+             "geo_local_area": "West End"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.118741, 49.276952]
+         },
+         "properties": {
+             "address": "911 Mainland St.",
+             "lot_operator": "City of Vancouver",
+             "geo_local_area": "Downtown"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.1239, 49.26036]
+         },
+         "properties": {
+             "address": "1580 W. Broadway",
+             "lot_operator": "VSB",
+             "geo_local_area": "Fairview"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.100393, 49.264664]
+         },
+         "properties": {
+             "address": "1 Kingsway",
+             "lot_operator": "Easypark",
+             "geo_local_area": "Mount Pleasant"
+         }
+     }, {
+         "type": "Feature",
+         "geometry": {
+             "type": "Point",
+             "coordinates": [-123.033786, 49.281369]
+         },
+         "properties": {
+             "lot_operator": "City of Vancouver",
+             "geo_local_area": "Hastings-Sunrise"
+         }
+     }]
+ };
 
-function successLocation(position) {
-    setupMap([position.coords.longitude, position.coords.latitude])
-}
+ var map = new mapboxgl.Map({
+     container: 'map',
+     style: "mapbox://styles/mapbox/streets-v11",
+     center: [-123.116226, 49.246292],
+     zoom: 3
+ });
 
-function errorLocation() {
-    setupMap([-123.116226, 49.246292])
-}
+ map.addControl(
+     new mapboxgl.GeolocateControl({
+         positionOptions: {
+             enableHighAccuracy: true
+         },
+         trackUserLocation: true
+     })
+ );
 
-function setupMap(center) {
-    const map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/streets-v11",
-        center: center,
-        zoom: 16.5
-    })
+ // add markers to map
+ geojson.features.forEach(function (marker) {
+     // create a HTML element for each feature
+     var el = document.createElement('div');
+     el.className = 'marker';
 
-    const nav = new mapboxgl.NavigationControl()
-    map.addControl(nav)
-}
+     // make a marker for each feature and add it to the map
+     new mapboxgl.Marker(el)
+         .setLngLat(marker.geometry.coordinates)
+         .setPopup(
+             new mapboxgl.Popup({
+                 offset: 25
+             }) // add popups
+             .setHTML(
+                 '<h3>' +
+                 marker.properties.address +
+                 '</h3><p>' +
+                 'Lot Operator: ' + marker.properties.lot_operator +
+                 '</p>'
+             )
+         )
+         .addTo(map);
+ });
