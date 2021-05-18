61fe862f5d3d638a8d47ae216d211413496724e9
SungJae Hong <sungjaehong10@gmail.com>
2021-05-18 16:05:37 -0700

    chat script

---
+++ js/chat.js
@@ -0,0 +1,42 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDT0wNqbuoiZEmUbJc4MFA_5l4kedZbTjU",
+    authDomain: "chargeup-4b09d.firebaseapp.com",
+    projectId: "chargeup-4b09d",
+    storageBucket: "chargeup-4b09d.appspot.com",
+    messagingSenderId: "141565730249",
+    appId: "1:141565730249:web:71710642aeb96d9ba11c18",
+    measurementId: "G-9QGMK8QKGR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// function sender() {
+//     var myName = document.getElementById("userInput").value;
+// }
+$('#submitButton').click(function () {
+    $('.nameInput').hide();
+});
+// var myName = prompt("Enter your name");
+
+function getName() {
+    var myName = document.getElementById("userInput").value;
+    firebase.database().ref("messages").push().set({
+        "sender": myName,
+        "message": "has entered the chat!"
+    });
+    // // get message
+    // var message = document.getElementById("message").value;
+
+    // // save in database
+    // firebase.database().ref("messages").push().set({
+    //     "sender": myName,
+    //     "message": message
+    // });
+}
+
+// const name = () => document.getElementByID("userInput").value;
+$("form").submit((e) => {
+    e.preventDefault();
+});
