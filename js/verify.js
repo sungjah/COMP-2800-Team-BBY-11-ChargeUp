11c4390097c6b0d2f53e4529f6f8a9f922caa9ab
Jacob Seol <jacobseol96@gmail.com>
2021-05-20 11:01:11 -0700

    Private chargers now display on map.

--- js/verify.js
+++ js/verify.js
@@ -2,32 +2,27 @@ function submit() {
     document.getElementById("submit").addEventListener('click', function () {
         let address = document.getElementById("address").value;
         console.log(address);
-        let unit = document.getElementById("unit").value;
-        console.log(unit);
+        let additional = document.getElementById("additional").value;
+        console.log(additional);
         let postalcode = document.getElementById("postalcode").value;
         console.log(postalcode);
+        let city = document.getElementById("city").value;
+        console.log(city);
+        let province = document.getElementById("province").value;
+        console.log(province);
+        let country = document.getElementById("country").value;
+        console.log(country);
 
-        firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                // User is signed in.
-                // Do something for the user here. 
-                console.log(user.uid);
-                db.collection("users").doc(user.uid)
-                    .get()
-                    .then(function (doc) {
-                        var usersRef = db.collection("users").doc(user.uid);
-                        usersRef.set({
-                            address: address,
-                            unit: unit,
-                            postalcode: postalcode
-                        }, {
-                            merge: true
-                        })
-                    })
-            } else {
-                // No user is signed in.
-            }
+        var verifyRef = db.collection("verify");
+        verifyRef.add({ //The us of curly brackets creates an object. We must use ":" to assign values to properties in an object.
+            address: address,
+            unit: additional,
+            postalcode: postalcode,
+            city: city,
+            province: province,
+            country: country
         });
+
     })
 }
 //submit();
